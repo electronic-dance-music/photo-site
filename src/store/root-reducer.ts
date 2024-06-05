63bab9b2402dbf79b906d07b5d4d853726e968de
electronic-dance-music <elizabethmdenham@gmail.com>
2024-06-04 19:36:38 -0500

    Updates

--- src/store/root-reducer.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import paginationReducer from "./pagination/pagination.reducer";
-import userReducer from "./user/user.reducer";
-
-const rootReducer = combineReducers({pagination: paginationReducer, user: userReducer})
-
-export default rootReducer
